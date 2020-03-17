4e26bbae8c4775236c97c9d3183fca6980472d55
tuanngoc92 <tuanngocmobileapp@gmail.com>
2020-03-17 21:52:00 +0700

    button image pressed,enable

--- app/src/main/java/com/example/designbutton/MainActivity.java
+++ app/src/main/java/com/example/designbutton/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.designbutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnCustom;
+    Switch swEnable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnCustom = findViewById(R.id.btn_custom);
+        swEnable= findViewById(R.id.sw_enable);
+        btnCustom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Click",Toast.LENGTH_LONG).show();
+            }
+        });
+        swEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked)
+                {
+                    btnCustom.setEnabled(true);
+                }
+                else
+                {
+                    btnCustom.setEnabled(false);
+                }
+            }
+        });
     }
 }
